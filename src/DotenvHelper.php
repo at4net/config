d2c70b1f039c404ff9aca874fcc8858f6cb9afc2
Daniel Sánchez <dsanchez@at4.net>
2019-07-19 14:26:08 +0200

    Prevent possible double declaration of the env function.

--- src/DotenvHelper.php
+++ src/DotenvHelper.php
@@ -1,10 +1,13 @@
 <?php
+if (! function_exists('env')) {
 
-function env($key, $default = null)
-{
-    $dotenv = \Sinergi\Config\Configuration::$env;
-    if (is_array($dotenv) && isset($dotenv[$key])) {
-        return $dotenv[$key];
+    function env($key, $default = null)
+    {
+        $dotenv = \Sinergi\Config\Configuration::$env;
+        if (is_array($dotenv) && isset($dotenv[$key])) {
+            return $dotenv[$key];
+        }
+        return $default;
     }
-    return $default;
+    
 }
